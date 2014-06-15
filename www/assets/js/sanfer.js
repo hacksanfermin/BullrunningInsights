a6166d7f0658e20866649e77dd4b4ffdde3c911f
Álvaro Sánchez <alvaro.sperez@gmail.com>
2014-06-15 12:00:16 +0200

    final version

--- www/assets/js/sanfer.js
+++ www/assets/js/sanfer.js
@@ -22,5 +22,20 @@ jQuery(document).ready(function($) {
 			} );
 		}
 		return false;
+	});	
+
+	// Switch section
+	$("#show-more").click(function() 
+	{
+		if( ! $(this).hasClass('active') ) { 
+			current_item = this;
+			// close all visible divs with the class of .section
+			$('.section:visible').fadeOut( section_hide_time, function() { 
+				$('a', '.mainmenu').removeClass( 'active' );  
+				var new_section = $( $(current_item).attr('href') );
+				new_section.fadeIn( section_show_time );
+			} );
+		}
+		return false;
 	});		
 });
